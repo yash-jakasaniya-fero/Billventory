7ef7b5f7ec21e1101192a589ea07e4933e816ee2
Jagat Panchal <jagat.panchal@fero.ai>
2025-04-24 00:53:10 +0530

    Registration Page changes

---
+++ frontend/src/views/ThankYou.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="text-center py-10">
+    <v-icon color="success" size="64">mdi-check-circle</v-icon>
+    <h1 class="text-h4 mt-4">Thank You for Registering!</h1>
+    <p class="text-subtitle-1 mt-2">
+      Redirecting you to the login page shortly...
+    </p>
+  </v-container>
+</template>
+  
+  <script setup>
+</script>
