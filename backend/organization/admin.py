87b207987d0870ceb25ccd5eab37d94158b4ca12
Jagat Panchal <jagat.panchal@fero.ai>
2025-04-24 01:02:47 +0530

    Admin Panel Changes

--- backend/organization/admin.py
+++ backend/organization/admin.py
@@ -1,24 +1,40 @@
 from django.contrib import admin
-from django.db import models
-from .models import Organization, OrganizationUser, OrganizationSubscription, OrganizationInventory, OrganizationProduct, OrganizationBilling, OrganizationBillingItem, OrganizationPurchaseItem,OrganizationPurchaseOrder, Supplier, OrganizationOnBoarding
+from .models import (
+    Organization,
+    OrganizationUser,
+    OrganizationSubscription,
+    OrganizationInventory,
+    OrganizationProduct,
+    OrganizationBilling,
+    OrganizationBillingItem,
+    OrganizationPurchaseItem,
+    OrganizationPurchaseOrder,
+    Supplier,
+    OrganizationOnBoarding,
+)
 
-@admin.register(models.OrganizationOnBoarding)
+@admin.register(OrganizationOnBoarding)
 class OrganizationOnboardingAdmin(admin.ModelAdmin):
     list_display = (
         "email",
         "otp",
         "is_verified",
-        "user_first_name", "user_last_name", "org_name", "org_address"
+        "user_first_name",
+        "user_last_name",
+        "org_name",
+        "org_address",
     )
-
     search_fields = ("email", "user_first_name", "user_last_name", "org_name")
-    list_filter ="is_verified"
-# Register your models here.
-admin.site.register(OrganizationOnboardingAdmin, OrganizationOnBoarding)
+    list_filter = ("is_verified",)  # <-- was a string before, should be a tuple
+
+# Register remaining models
 admin.site.register(Organization)
 admin.site.register(OrganizationUser)
 admin.site.register(OrganizationSubscription)
 admin.site.register(OrganizationInventory)
+admin.site.register(OrganizationProduct)
 admin.site.register(OrganizationBilling)
+admin.site.register(OrganizationBillingItem)
+admin.site.register(OrganizationPurchaseItem)
 admin.site.register(OrganizationPurchaseOrder)
 admin.site.register(Supplier)
