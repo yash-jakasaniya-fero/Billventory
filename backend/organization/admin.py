174f429d73e49f249037f42a5edd16f0580aefcf
Jagat Panchal <jagat.panchal@fero.ai>
2025-04-24 00:59:02 +0530

    Admin Panel Changes

--- backend/organization/admin.py
+++ backend/organization/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
+from django.db import models
 from .models import Organization, OrganizationUser, OrganizationSubscription, OrganizationInventory, OrganizationProduct, OrganizationBilling, OrganizationBillingItem, OrganizationPurchaseItem,OrganizationPurchaseOrder, Supplier, OrganizationOnBoarding
 
+@admin.register(models.OrganizationOnBoarding)
+class OrganizationOnboardingAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "otp",
+        "is_verified",
+        "user_first_name", "user_last_name", "org_name", "org_address"
+    )
+
+    search_fields = ("email", "user_first_name", "user_last_name", "org_name")
+    list_filter ="is_verified"
 # Register your models here.
-admin.site.register(OrganizationOnBoarding)
+admin.site.register(OrganizationOnboardingAdmin, OrganizationOnBoarding)
 admin.site.register(Organization)
 admin.site.register(OrganizationUser)
 admin.site.register(OrganizationSubscription)
